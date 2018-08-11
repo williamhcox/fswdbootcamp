a355eda9aaef15d386c052ad1258640fa5ca6505
williamhcox <42050215+williamhcox@users.noreply.github.com>
2018-08-10 21:36:04 -0700

    jquery progress 1

---
+++ Exercises/jQuery/ex1/exercise.js
@@ -0,0 +1,4 @@
+$("div").css("background", "purple");
+$("div.highlight").css("width", "200px");
+$("div#third").css("border", "orange 1px solid");
+$("div:nth-of-type(1)").css("background", "pink");
